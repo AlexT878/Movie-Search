87278c09721bbcb4d20be22277a14fb2b7b27318
Tiganete Alexandru <Alexandru.Tiganete@cognizant.com>
2026-02-17 11:19:40 +0200

    feature: added filter and sorting trough url

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import moviesData from "./assets/movies.json"
 import MovieCard from "./components/MovieCard.JSX"
 import MovieDetails from "./components/MovieDetails"
 
-import { useState } from "react"
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useSearchParams } from "react-router-dom";
 
 function App() {
-  const [searchedMovie, setSearchedMovie] = useState('');
-  const [selectedGenre, setSelectedGenre] = useState('All Genres')
-  const [selectedSortOption, setSelectedSortOption] = useState('Alphabetical: A-Z')
+  const [searchParams, setSearchParams] = useSearchParams();
+  const searchedMovie = searchParams.get("search") || "";
+  const selectedGenre = searchParams.get("genre") || "All Genres";
+  const selectedSortOption = searchParams.get("sort") || "Alphabetical: A-Z";
 
   const filteredMovies = moviesData.filter((movie) => {
     const matchesName = movie.title.toLowerCase().includes(searchedMovie.toLowerCase())
@@ -27,15 +27,37 @@ function App() {
     return 0;
   })
 
+  function updateURL(newFilters) {
+    const nextParams = {
+      search: searchedMovie,
+      genre: selectedGenre,
+      sort: selectedSortOption,
+      ...newFilters
+    }
+    if (!nextParams.search) {
+      delete nextParams.search;
+    }
+
+    if (nextParams.genre === "All Genres") {
+      delete nextParams.genre;
+    }
+
+    if (nextParams.sort === "Alphabetical: A-Z") {
+      delete nextParams.sort;
+    }
+
+    setSearchParams(nextParams);
+  }
+
   return (
     <>
       <Header 
         searchedMovie={searchedMovie} 
-        setSearchedMovie={setSearchedMovie} 
+        setSearchedMovie={(val) => updateURL({ search: val })} 
         selectedGenre={selectedGenre} 
-        setSelectedGenre={setSelectedGenre}
+        setSelectedGenre={(val) => updateURL({ genre: val })}
         selectedSortOption={selectedSortOption}
-        setSelectedSortOption={setSelectedSortOption}
+        setSelectedSortOption={(val) => updateURL({ sort:val })}
       />
       <main className="main-content">
         <Routes>
